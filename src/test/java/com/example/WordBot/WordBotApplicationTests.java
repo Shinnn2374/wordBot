42e015197ec83b85c45dd98cc0914ccc4e1abf1d
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2025-03-06 21:16:44 +0300

    ...

---
+++ src/test/java/com/example/WordBot/WordBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WordBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
