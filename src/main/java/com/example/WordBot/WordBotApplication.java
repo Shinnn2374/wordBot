a038e156217f87b6843f1fc773de1ee6446aa660
Shinnn2374 <168295007+Shinnn2374@users.noreply.github.com>
2025-03-06 12:35:18 +0300

    ...

---
+++ src/main/java/com/example/WordBot/WordBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.WordBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordBotApplication.class, args);
+	}
+
+}
